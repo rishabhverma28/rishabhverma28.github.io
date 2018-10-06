b812ee35093db3d0fecea7c96a57e964504f6af6
Rishabh Verma <rishabhverma.2806@gmail.com>
2018-10-06 17:47:07 +1000

    React Upgrade

---
+++ src/App.js
@@ -0,0 +1,342 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import LogoImage from './gent.svg';
+import dict from './data.json'
+import './App.css';
+import BarChart from './BarChart'
+
+class MyDiv extends Component{
+    render(){
+        return (
+            <div>Hello!</div>
+            )
+    }
+}
+
+class LogoImg extends Component {
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return (
+            <div className = "site-logo">
+                <img src = {LogoImage} className = "site-logo-img"/>
+            </div>
+            )
+    }
+}
+
+// class MenuItem extends Component{
+//     render(){
+//         return(
+//                 <li>{}</li>
+//             )
+//     }
+// }
+
+// class Menu extends Component{
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//             error: null,
+//             isLoaded: false,
+//             items: []
+//         };
+//     }
+
+//     componentDidMount() {
+//         // fetch('./data.json')
+//         //     .then(res => res.text())
+//         //     .then(text => console.log(text))
+//         //     .then(
+//         //         (result) => {
+//         //             this.setState({
+//         //                 isLoaded:true,
+//         //                 items: result.items
+//         //             });
+//         //         },
+//         //         (error) => {
+//         //             this.setState({
+//         //                 isLoaded:true,
+//         //                 error
+//         //             });
+//         //         }
+//         //     )
+//         let results =dict.map((item, index) => {
+//             console.log(item, index)
+//         });
+//     }
+//     render(){
+//         const{error, isLoaded, items} = this.state;
+//         if(error) {
+//             return <div>Error: {error.message}</div>;
+//         } else if (!isLoaded){
+//             return <div>Loading ...</div>;
+//         } else {
+//             return(
+//                 <ul className = "menu">
+//                     {items.map(item => (
+//                         <li key = {item.name}>
+//                             {item.name}
+//                         </li>
+//                         ))}
+//                 </ul>
+
+//             )
+//         }
+        
+//     }
+// }
+
+// class MenuItems extends Components{
+
+// }
+
+class Menu extends Component{
+    render(props){
+        const dataItems = this.props.dict.menuItems;
+        const listItems = dataItems.map((item)=>
+                <li><a href = {item.link}>{item.name}</a></li>
+            )
+        return(
+                // <div className="menu-container">
+                <ul className="menu-items">
+                    {listItems}
+                </ul>
+                // </div>
+            )
+    }
+}
+
+class AsideMenu extends Component {
+    constructor(props){
+        super(props);
+        this.state = {toggle:false};
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(e){
+        this.setState(state =>({
+            toggle: !state.toggle
+        }))
+        // if(this.state.toggle){
+        //     document.querySelector(".root").style.position = "static"
+        //     document.querySelector(".site-hero").style.opacity = "1"
+        // }
+        // else{
+        //     document.querySelector(".root").style.position = "fixed"
+        //     document.querySelector(".site-hero").style.opacity = "0.4"
+        //     // document.querySelector(".aside-bar").style.opacity = "1"
+        // }
+        // console.log("clicked", this.state.toggle);
+        // if(this.state.toggle){
+
+        // }
+    }
+    render(){
+        const style = {display: this.state.toggle ? 'block' : 'none' }
+        const tt = this.state.toggle
+        // const name = {tt? "on":null}
+        return(
+            <aside className="aside-bar">
+                {/* <div className = "menu"> */}
+                    <div className = {`trigram-icon ${tt}`}  onClick ={this.handleClick}></div>
+                    <div className = {`show-hide ${tt}`} style = {style}>
+                        <Menu dict = {dict}/>
+                    </div>
+                    
+                {/* </div> */}
+                
+            </aside>
+        )
+    }
+}
+
+class NavBar extends Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return (
+            <nav className = "site-navbar">
+                <LogoImg />
+                <AsideMenu />
+            </nav>
+            )
+    }
+}
+
+class CircleSvg extends Component{
+    render(){
+        return(
+                // <div className="svg-circle">
+                    // <svg xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="100" cy="100" r={this.props.radius} fill={this.props.circlecolor} className = {this.props.name}/>
+                    // </svg>
+                // </div>
+            )
+    }
+}
+
+class LineSvg extends Component{
+    render(){
+        return (
+                // <svg xmlns="http://www.w3.org/2000/svg">
+                        <line x1="100" y1="0" x2="100" y2="100" stroke="black"/>
+                    // </svg>
+            )
+    }
+}
+class CompleteSvg extends Component{
+    render(){
+        return (
+                <div className= "svg-line">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="100%">
+                    <g className="grouped-svg">
+                        <LineSvg />
+                        <CircleSvg radius = "10" circlecolor="#fdd835" name = "smaller-circle"/>
+                        <CircleSvg radius = "20" circlecolor="#fffde7" name = "bigger-circle"/>
+                    </g>
+                    </svg>
+                </div>
+
+            )
+    }
+} 
+
+class Hero extends Component{
+    render() {
+        console.log(dict)
+        return (
+            
+            <div className = "site-hero">
+                <CompleteSvg/>
+                <CompleteSvg/>
+                <div className = "hero-welcome">Welcome.</div>
+                <div className = "hero-pipe"></div>
+                <CompleteSvg/>
+                <CompleteSvg/>
+            </div>
+            
+            )
+    }
+}
+
+class Picture extends Component{
+    render(){
+        return (
+            <img src={require(`${this.props.srcImg}`)}/>   
+        )
+    }
+}
+
+class Description extends Component{
+    render(){
+        console.log(this.props)
+        return(
+            <div className="description">{this.props.description}</div>
+        )
+    }
+}
+
+class TimelineB extends Component{
+    // constructor(props){
+    //     super(props)
+    // }
+    render(){
+        console.log(this.props.data.year);
+        return(
+            // "HI"
+            <div className="bar-container">
+                <div className="year">{this.props.data.year}</div>
+                <div className="time-desc">{this.props.data.description}</div>
+            </div>
+        )
+    }
+}
+
+class Timeline extends Component{
+    render(){
+        const data = this.props.info
+        console.log(data)
+        
+            return this.props.info.map((comp)=>{
+                console.log(comp);
+                return (<TimelineB data={comp}/>)
+            })
+        
+    }
+}
+
+class AboutMe extends Component{
+    render(){
+        console.log("ggh",dict.aboutMe[0].imagesrc)
+
+        return(
+            <section className = "about-me" id = "about-me">
+                <div className="section-header">About Me</div>
+                <div className="about-me-wrapper clearfix">
+                    <Description description={dict.aboutMe[1].description}/>                
+                    <Picture srcImg={dict.aboutMe[0].imagesrc}/>
+                </div>
+                <div className="timeline-wrapper clearfix">
+                    <Timeline info= {dict.timeline}/>
+                </div>
+            </section>
+        )
+    }
+}
+
+class ContactMe extends Component{
+    render(){
+
+        return this.props.data.map((comp)=>{
+            console.log(comp);
+            return (<div className={`social ${comp.name}`}>
+                    <a href={comp.link} target="_blank"><img src={require(`${comp.imageSrc}`)}/></a>
+                </div>)
+        })
+    }
+}
+
+class App extends Component {
+    render(){
+        return (
+            <div className="root">
+                <NavBar/>
+                <Hero />
+                <AboutMe  />
+                <section className="technologies">
+                    <div className="section-header" id="tech-header">Technologies</div>
+                    <BarChart data={dict.technologies} size={[300,300]} />
+                </section>
+                <section className="contact-me" id="contact-me">
+                    <div className="section-header">Contact Me</div>
+                    <ContactMe data= {dict.contactMe}/>
+                </section>
+            </div>
+            )
+    }
+}
+
+// class App extends Component {
+//     constructor(props){
+//         super(props)
+//     }
+//     render() {
+//         return (
+//             // <MyDiv />,
+//             <div className="App">
+//                 <header className="App-header">
+//                     <img src={logo} className="App-logo" alt="logo" />
+//                     <h1 className="App-title">Welcome to React</h1>
+//                 </header>
+//                 <p className="App-intro">
+//                   To get started, edit <code>src/App.js</code> and save to reload.
+//                 </p>
+//                 <MyDiv />
+//             </div>
+//         );
+//         }
+// }
+
+export default App;
---
+++ src/BarChart.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react'
+import './App.css'
+import { scaleLinear, scaleBand } from 'd3-scale'
+import { max } from 'd3-array'
+import { select } from 'd3-selection'
+import { axisLeft, axisBottom } from 'd3-axis'
+
+class BarChart extends Component {
+   constructor(props){
+      super(props)
+      this.createBarChart = this.createBarChart.bind(this)
+   }
+   componentDidMount() {
+      this.createBarChart()
+   }
+   componentDidUpdate() {
+      this.createBarChart()
+   }
+   createBarChart() {
+      const margin = {top:20, right:20, bottom:20, left:50}
+      const width = this.props.size[0]- margin.left - margin.right
+      const height = this.props.size[1] - margin.bottom - margin.top 
+      const node = this.node
+      let svg = select(node)
+            .append("svg")
+            .attr("width", width+ margin.left + margin.right)
+            .attr("height", height+ margin.bottom + margin.top )
+            .append("g")
+                  .attr("transform", "translate(" + margin.left + ","+ margin.right + ")")
+      // const dataMax = max(this.props.data,(d)=>d.value)
+      const yScale = scaleBand()
+         .domain(this.props.data.map((d) => d.name)).padding(0.5)
+         .range([height, 0])
+      const xScale = scaleLinear()
+         .domain([0, 100])
+         .range([0, width])
+      
+   svg
+      .selectAll('.bars')
+      .data(this.props.data)
+      .enter()
+      .append('rect')
+
+   svg
+      .append('g')
+      .call(axisLeft(yScale))
+   svg
+      .append('g')
+      .call(axisBottom(xScale))
+      .attr('transform', 'translate(0,'+ height +')')
+
+   // svg
+   //    .selectAll('rect')
+   //    .data(this.props.data)
+   //    .exit()
+   //    .remove()
+   console.log("ffff",yScale.bandwidth())
+   svg
+      .selectAll('rect')
+      .data(this.props.data)
+      
+      .style('fill', '#fe9922')
+      .attr('x', 0)
+      .attr('y', d => yScale(d.name))
+      .attr('height', d => yScale.bandwidth())
+      // .attr('margin','5px')
+      // .attr("width", 0)
+      // .transition()
+      // .duration(1500)
+      .attr('width', d=> xScale(d.value))
+svg
+      .selectAll("text.label")
+      .data(this.props.data)
+      .enter()
+      .append("text")
+      .attr("class", "label")
+      .attr("y", d=>yScale(d.name)+yScale.bandwidth()/2+4)
+      .attr("x", d=>xScale(d.value)+3)
+      .text(d=>d.value)
+
+   }
+render() {
+      return (
+            <div className="chart" ref={node => this.node = node}>
+            </div>
+      )
+   }
+}
+export default BarChart
