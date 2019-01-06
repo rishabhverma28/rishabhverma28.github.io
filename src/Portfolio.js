d18ce74405ce1df846dd5c2ed149e95a62fc3921
Rishabh Verma <rishabhverma.2806@gmail.com>
2019-01-06 12:54:49 +0530

    Adding Resume, Contact Me pages and their styles

---
+++ src/Portfolio.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Portfolio extends Component {
+    render(){
+        return(
+            <div className="cmn">
+                <span>This is the Portfolio of Components that I have created in React.</span>
+            </div>
+        )
+    }
+
+}
+
+export default Portfolio;
